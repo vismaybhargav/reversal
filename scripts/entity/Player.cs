4c5245fef9ba4ee7d86de6e17e7baaf13a12f53c
Dhruva Srinivas <dhruvas17068@gmail.com>
2024-12-27 13:32:31 -0800

    rotation

--- scripts/entity/Player.cs
+++ scripts/entity/Player.cs
@@ -5,13 +5,20 @@ namespace reversal.scripts.entity;
 
 public partial class Player : CharacterBody2D
 {
-    public override void _Ready()
-    {
-        // INIT HERE
-    }
+	public Vector2 ScreenSize;
 
-    public override void _Process(double delta)
-    {
-          
-    }
+	public override void _Ready()
+	{
+		// INIT HERE
+		ScreenSize = GetViewportRect().Size; // we might need this later? lol
+	}
+
+	public override void _Process(double delta)
+	{
+		Vector2 mousePosition = GetGlobalMousePosition();
+		Vector2 direction = mousePosition - GlobalPosition;
+		float angle = Mathf.Atan2(direction.Y, direction.X);
+
+		Rotation = angle;
+	}
 }
