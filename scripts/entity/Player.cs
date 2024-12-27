35768d28fe70a8ecc4708dd4fc4dd9f7d9f345a8
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-12-27 14:54:33 -0800

    (cleanup) code cleanup + refactor

--- scripts/entity/Player.cs
+++ scripts/entity/Player.cs
@@ -7,49 +7,45 @@ public partial class Player : CharacterBody2D
 {
 	[Export] public int Speed { get; set; } = 400;
 	
-	public Vector2 ScreenSize;
-
-	public const int PLAYER_SPEED_INCREMENT = 1;
+	private Vector2 _screenSize;
+	
+	[Export]
+	public int PlayerSpeedIncrement = 1;
 
 	public override void _Ready()
 	{
 		// INIT HERE
-		ScreenSize = GetViewportRect().Size; // we might need this later? lol
-		Position = new Vector2(200, 300);
+		_screenSize = GetViewportRect().Size; // we might need this later? lol
+		//Position = new Vector2(200, 300);
 	}
 
 	public override void _Process(double delta)
 	{
 		// Rotation
-		Vector2 mousePosition = GetGlobalMousePosition();
-		Vector2 direction = mousePosition - GlobalPosition;
-		float angle = Mathf.Atan2(direction.Y, direction.X);
-
-		Rotation = angle;
+		Rotation = GetAngleToMouseRad();
 		
 		// WASD movements
 		var velocity = Vector2.Zero;
 
 		if (Input.IsActionPressed("move_up"))
 		{
-			velocity.Y -= PLAYER_SPEED_INCREMENT;
+			velocity.Y -= PlayerSpeedIncrement;
 		}
 		
 		if (Input.IsActionPressed("move_down"))
 		{
-			velocity.Y += PLAYER_SPEED_INCREMENT;
+			velocity.Y += PlayerSpeedIncrement;
 		}
 				
 		if (Input.IsActionPressed("move_left"))
 		{
-			velocity.X -= PLAYER_SPEED_INCREMENT;
+			velocity.X -= PlayerSpeedIncrement;
 		}
 						
 		if (Input.IsActionPressed("move_right"))
 		{
-			velocity.X += PLAYER_SPEED_INCREMENT;
+			velocity.X += PlayerSpeedIncrement;
 		}
-
 
 		if (velocity.Length() > 0)
 		{
@@ -57,9 +53,20 @@ public partial class Player : CharacterBody2D
 		}
 
 		Position += velocity * (float)delta;
+		
 		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+			x: Mathf.Clamp(Position.X, 0, _screenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, _screenSize.Y)
 		);
+	}
+	
+	/// <summary>
+	/// Calculates the angle between current rotation and the mouse
+	/// </summary>
+	/// <returns>the angle in radians</returns>
+	private float GetAngleToMouseRad()
+	{
+		var direction = GetGlobalMousePosition() - GlobalPosition;
+		return Mathf.Atan2(direction.Y, direction.X);	
 	}
 }
--- scripts/world/Main.cs
+++ scripts/world/Main.cs
@@ -1,20 +1,32 @@
 using Godot;
-using System;
 
-namespace reversal.scripts;
+namespace reversal.scripts.world;
 
 public partial class Main : Node2D
 {
 	private CharacterBody2D _player;
+	private Camera2D _camera;
 	
 	public override void _Ready()
 	{
-		// INIT HERE
+		InstantiateNodes();
+		
+		_camera.MakeCurrent();
+	}
+	
+	/// <summary>
+	/// Instantiates all the child nodes
+	/// </summary>
+	public void InstantiateNodes()
+	{
 		_player = GetNode<CharacterBody2D>("Player");
+		_camera = GetNode<Camera2D>("Player/Camera2D");
 	}
 	
 	public override void _Process(double delta)
 	{
+		_camera.Position = _player.Position;
+			
 		if (Input.IsActionPressed("ui_accept"))
 		{
 			GD.Print("Accept button pressed");
