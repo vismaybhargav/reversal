68d19b90d7141c36eee2666e01d9fd3b001036fa
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-12-28 23:52:57 -0800

    (feat) change camera shake based on bullet type

--- scripts/util/PlayerCamera.cs
+++ scripts/util/PlayerCamera.cs
@@ -1,19 +1,14 @@
 using Godot;
+using reversal.scripts.entity.bullet;
 
 namespace reversal.scripts.util;
 
 public partial class PlayerCamera : Camera2D
 {
-	[Export] public double RandomStrength = 30.0;
 	[Export] public double ShakeFade = 5.0;
 
 	private RandomNumberGenerator _rand = new();
 	private double _shakeStrength;
-	
-	private void ApplyShake()
-	{
-		_shakeStrength = RandomStrength;
-	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -23,7 +18,11 @@ public partial class PlayerCamera : Camera2D
 		_shakeStrength = Mathf.Lerp(_shakeStrength, 0, ShakeFade * delta);
 		Offset = GetRandomOffset();
 	}
-
+	private void ApplyShake(double strength)
+	{
+		_shakeStrength = strength;
+	}
+	
 	private Vector2 GetRandomOffset()
 	{
 		return new Vector2(
@@ -31,8 +30,9 @@ public partial class PlayerCamera : Camera2D
 			_rand.RandfRange((float)-_shakeStrength, (float)_shakeStrength));
 	}
 
-	private void OnPlayerShoot(PackedScene bullet, Vector2 pos, Vector2 dir) 
-	{ 
-		ApplyShake();
+	private void OnPlayerShoot(PackedScene bullet, Vector2 pos, Vector2 dir)
+	{
+		// Get each bullet's camera shake  value
+		ApplyShake(((Bullet)bullet.Instantiate()).CameraShakeIntensity);
 	}
 }
