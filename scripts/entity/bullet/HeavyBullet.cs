6b6b88a814617d4af279c7e01e544165e606bebe
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-12-28 23:56:51 -0800

    (wip) add deviation to heavy bullet movement

---
+++ scripts/entity/bullet/HeavyBullet.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+namespace reversal.scripts.entity.bullet;
+
+public partial class HeavyBullet : Bullet
+{
+    
+    [Export] public double DeviationIntensityRadians = 0.5;
+    private RandomNumberGenerator _rand = new();
+
+    public override void _Ready()
+    {
+        GlobalRotation += _rand.RandfRange((float)-DeviationIntensityRadians, (float)DeviationIntensityRadians);
+    }
+}
