f2f42dc8bc068c02de5aed0fa4331c89cb2dc485
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-12-28 23:54:36 -0800

    (feat) show debug info on sep canvas layer

---
+++ scripts/ui/dev_debug/DebugInfo.cs
@@ -0,0 +1,51 @@
+using Godot;
+using reversal.scripts.entity;
+using reversal.scripts.world;
+
+namespace reversal.scripts.ui.dev_debug;
+public partial class DebugInfo : CanvasLayer
+{
+	private Label _label;
+	private Main _main;
+	private Player _player;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		InstantiateNodes();
+	}
+
+	private void InstantiateNodes()
+	{
+		_label = GetNode<Label>("Label");
+		_main = (Main)GetParent();
+		_player = (Player)_main.GetChild(1);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		_label.Text = $"""
+		               DEBUG_BUILD - DEVELOPER: {MatchDev(OS.GetName())}
+		               FPS: {Engine.GetFramesPerSecond()}
+		               Architecture: {Engine.GetArchitectureName()}
+		               OS: {OS.GetName()} {OS.GetVersion()}
+		               CPU: {OS.GetProcessorName()} 
+		               Cores: {OS.GetProcessorCount()}
+		               GPU: {RenderingServer.GetRenderingDevice().GetDeviceName()}
+		               Player Pos: {_player.Position}
+		               Bullet Type: {_player.BulletScene.Instantiate().GetName()}
+		               """;
+	}
+
+	private static string MatchDev(string osName)
+	{
+		return osName switch
+		{
+			"Windows" => "Vismay",
+			"macOS" => "Jaewon",
+			"Linux" => "Dhruva",
+			_ => "Unknown"
+		};
+	}
+}
