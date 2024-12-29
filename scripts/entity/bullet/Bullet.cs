bf9d06a4a585547be9a10a5399eaceecdb432f37
Vismay Bhargav <vismay.bhargav@gmail.com>
2024-12-28 23:55:02 -0800

    (cleanup) bullet code clean + format

--- scripts/entity/bullet/Bullet.cs
+++ scripts/entity/bullet/Bullet.cs
@@ -3,28 +3,31 @@ using Godot;
 namespace reversal.scripts.entity.bullet;
 public partial class Bullet : Area2D
 {
-	[Export] public int BulletSpeedIncrement = 2500;
-	[Export] public float BulletLifeTime = 0.75f;
+	[Export] public int   BulletSpeed          = 2500;
+	[Export] public float BulletLifeTime       = 0.75f;
+	[Export] public int   CameraShakeIntensity = 5;
+	[Export] public float Damage               = 20;
+	
 	private Vector2 _velocity = Vector2.Zero;
 
 	// Child Nodes
 	private Timer _timer;
+	
+	private void InstantiateChildNodes()
+	{
+		_timer = GetNode<Timer>("Timer");
+	}
 
 	public override void _Ready()
 	{
 		InstantiateChildNodes();
 	}
 
-	private void InstantiateChildNodes()
-	{
-		_timer = GetNode<Timer>("Timer");
-	}
-
 	public void Start(Vector2 pos, Vector2 dir)
 	{
 		Position = pos;
 		Rotation = dir.Angle();
-		_velocity = dir * BulletSpeedIncrement;
+		_velocity = dir * BulletSpeed;
 	}
 
 	public override void _Process(double delta)
