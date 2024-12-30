af9a8aa2f25582de908dfd9840c79cb181be3570
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2024-12-29 22:35:09 -0800

    (clean) cleanup main code

--- scripts/world/Main.cs
+++ scripts/world/Main.cs
@@ -13,38 +13,39 @@ public partial class Main : Node2D
 		Positive,
 		Negative
 	}
-	
+
 	[Export] public int EnemyCount = 5;
 	[Export] public float EnemySpawnMinY = -450f;
 	[Export] public float EnemySpawnMaxY = 2240f;
-		
+
 	[Signal]
-	public delegate void PolaritySwitchEventHandler(int polarity); // 0 = Positive, 1 = Negative Godot being annoying with enums
+	public delegate void
+		PolaritySwitchEventHandler(int polarity); // 0 = Positive, 1 = Negative Godot being annoying with enums
 
 	public Polarity CurrentPolarity = Polarity.Positive;
-	
+
 	private CharacterBody2D _player;
 	private Timer _polaritySwitchCountdown;
 	private Camera2D _camera;
 	private CanvasLayer _dbgUi;
 	private TileMapLayer _positiveTileMap;
 	private TileMapLayer _negativeTileMap;
-	
+
 	private void InstantiateChildNodes()
 	{
-						 _player = GetNode<CharacterBody2D>("Player");
-						 _camera = GetNode<Camera2D>("Player/PlayerCamera");
-						 _dbgUi  = GetNode<CanvasLayer>("DBG_Info");
+		_player = GetNode<CharacterBody2D>("Player");
+		_camera = GetNode<Camera2D>("Player/PlayerCamera");
+		_dbgUi = GetNode<CanvasLayer>("DBG_Info");
 		_polaritySwitchCountdown = GetNode<Timer>("PolaritySwitchCountdown");
 	}
-	
+
 	public override void _Ready()
 	{
 		GD.Print("Main ready");
 		InstantiateChildNodes();
-		
-		if(!OS.IsDebugBuild()) _dbgUi.Hide();
-		
+
+		if (!OS.IsDebugBuild()) _dbgUi.Hide();
+
 		_camera.MakeCurrent();
 
 		for (var _ = 0; _ < EnemyCount; ++_) SpawnEnemy();
@@ -55,19 +56,19 @@ public partial class Main : Node2D
 		var enemyScene = GD.Load<PackedScene>("res://scenes/entity/enemy/Enemy.tscn");
 		var enemy = (Enemy)enemyScene.Instantiate();
 		AddChild(enemy);
-		
+
 		var random = new RandomNumberGenerator();
 		random.Randomize();
-		
+
 		Array<int> spawnZonesX = [-1440, -800, 1920, 1700];
 		var spawnX = spawnZonesX[random.RandiRange(0, spawnZonesX.Count - 1)];
-		
+
 		random.Randomize();
 		var spawnY = (int)random.RandfRange(EnemySpawnMinY, EnemySpawnMaxY);
-		
+
 		enemy.Position = new Vector2(spawnX, spawnY);
 	}
-	
+
 	private void OnPlayerShoot(PackedScene bullet, Vector2 pos, Vector2 dir)
 	{
 		var b = (Bullet)bullet.Instantiate();
@@ -78,12 +79,6 @@ public partial class Main : Node2D
 	private void OnPolaritySwitchCountdownTimeout()
 	{
 		CurrentPolarity = CurrentPolarity == Polarity.Positive ? Polarity.Negative : Polarity.Positive;
-		SwitchTilemap();
-		EmitSignal("PolaritySwitchEventHandler", (int)CurrentPolarity);
-	}
-	
-	private void SwitchTilemap() 
-	{
 		switch (CurrentPolarity)
 		{
 			case Polarity.Positive:
@@ -97,5 +92,7 @@ public partial class Main : Node2D
 			default:
 				throw new ArgumentOutOfRangeException();
 		}
+
+		EmitSignal("PolaritySwitchEventHandler", (int)CurrentPolarity);
 	}
 }
